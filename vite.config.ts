f3ab453a5f9063571de87fc8e2a57cc3fa76ddad
Fillon Mathias <mathias.fillon@gmail.com>
2026-05-13 17:13:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './',
+    base: '/dofuselevage/',
     build: {
       outDir: 'docs',
       emptyOutDir: true,
